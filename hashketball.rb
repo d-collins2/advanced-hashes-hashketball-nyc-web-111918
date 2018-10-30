ca3f585045b80d95fc400c41389f3a6b10211f4e
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-30 10:03:29 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,6 +126,7 @@ def num_points_scored(name)
     players_hash =  team_info_hash[:players]
     players_hash.each do |player_details_hash|
       if player_details_hash[:name] == name
+      end 
     end
   end
 end
